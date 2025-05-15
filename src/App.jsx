03df1fbc74cb11a54138dcc63cd9b9c353532325
Poshika Rajpurohit <7466poshikarajpurohit@gmail.com>
2025-05-15 13:32:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
-
 import './App.css'
 import Header from './Components/Header'
 import Add from './Components/Add';
 import {  Routes,Route } from 'react-router'
 import Home from './Components/Home';
 import EditPatient from './Components/Edit';
-// import PatientForm from './Components/Etc.JSX';
-
-
 function App() {
-
-
   return (
     <>
       <Header/>
@@ -20,9 +14,7 @@ function App() {
        <Route path="/" element={<Home />} />
        <Route path="/edit/:id" element={<EditPatient />} />
       </Routes>
-      {/* l<PatientForm/> */}
     </>
   )
 }
-
 export default App
